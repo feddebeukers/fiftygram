ba529ec187c68ca3defff33d238d2d37a4eebe9c
Fedde Beukres <57895611+feddebeukers@users.noreply.github.com>
2020-10-05 09:41:54 +0200

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,96 @@
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+
+    let context = CIContext()
+    var original: UIImage!
+    var filtered: UIImage!
+
+    @IBAction func applySepia() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        filter?.setValue(1.0, forKey: kCIInputIntensityKey)
+        display(filter: filter!)
+        filtered = original
+    }
+
+    @IBAction func applyNoir() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+        filtered = original
+    }
+
+    @IBAction func applyVintage() {
+        if original == nil {
+            return
+        }
+
+        let filter = CIFilter(name: "CIPhotoEffectProcess")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+        filtered = original
+    }
+    
+    @IBAction func applyChrome() {
+        if original == nil {
+            return
+        }
+        
+        let filter = CIFilter(name: "CIPhotoEffectChrome")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+        filtered = original
+        
+    }
+
+    @IBAction func choosePhoto(_ sender: UIBarButtonItem) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            self.navigationController?.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func savePhoto() {
+        UIImageWriteToSavedPhotosAlbum(filtered, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+
+    func display(filter: CIFilter) {
+        let output = filter.outputImage!
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output, from: output.extent)!)
+    }
+
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        self.navigationController?.dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            original = image
+        }
+    }
+}
